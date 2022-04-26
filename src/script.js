f54d6c717159eb115ec52949487406f8178ad67d
AlbertKovtoun <AlbertKovtoun>
2022-04-26 15:48:51 +0200

    Test

--- src/script.js
+++ src/script.js
@@ -1,2 +1,7 @@
+import p5 from "p5"
+import { setSketch } from "./javascript/p5/sketch"
+
 import "./style.scss"
 // import "./javascript/p5/sketch"
+
+new p5(setSketch, document.querySelector(".sketch"))
